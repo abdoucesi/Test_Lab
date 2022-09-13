56f1f92cf70a2a202cabe211800ecbddd95a7943
hamicheab <abderrahmane.hamiche@slib.com>
2022-09-13 14:11:00 +0200

    Commit que le second fichier

--- Second_File.java
+++ Second_File.java
@@ -1,6 +1,7 @@
 public  class Bonjour {  
 	 public  static  void main (String [] arguments) {  
 		System.out.println("Bonjour le monde") ;  
-		System.exit(0) ;  
+		System.exit(0);
+        System.out.println("Modification sur le second fichier");
 	}
 }
